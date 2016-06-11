0dde070a4bf194663cd43251c181a284c27e1e87
BossaGroove <bossagrooveaware@gmail.com>
2016-06-12 01:56:33 +0800

    Ad-hoc equipment transfer https://github.com/Grabacr07/KanColleViewer/commit/a4e303b29ba652cebbbc0f009ed61e3b11a52b6e

--- Grabacr07.KanColleWrapper/Organization.cs
+++ Grabacr07.KanColleWrapper/Organization.cs
@@ -138,6 +138,7 @@ namespace Grabacr07.KanColleWrapper
 			proxy.api_req_hokyu_charge.TryParse<kcsapi_charge>().Subscribe(x => this.Charge(x.Data));
 			proxy.api_req_kaisou_powerup.TryParse<kcsapi_powerup>().Subscribe(this.Powerup);
             proxy.api_req_kaisou_slot_exchange_index.TryParse<kcsapi_slot_exchange_index>().Subscribe(this.ExchangeSlot);
+            proxy.api_req_kaisou_slot_deprive.TryParse<kcsapi_slot_deprive>().Subscribe(x => this.DepriveSlotItem(x.Data));
             proxy.api_req_kousyou_getship.TryParse<kcsapi_kdock_getship>().Subscribe(x => this.GetShip(x.Data));
 			proxy.api_req_kousyou_destroyship.TryParse<kcsapi_destroyship>().Subscribe(this.DestoryShip);
 			proxy.api_req_member_updatedeckname.TryParse().Subscribe(this.UpdateFleetName);
@@ -379,9 +380,24 @@ namespace Grabacr07.KanColleWrapper
 
 		#endregion
 
-		#region 工廠 (Get / Destroy)
+        #region 改装
 
-		private void GetShip(kcsapi_kdock_getship source)
+        private void DepriveSlotItem(kcsapi_slot_deprive source)
+        {
+            if (this.Ships[source.api_ship_data.api_unset_ship.api_id] != null) {
+                this.Ships[source.api_ship_data.api_unset_ship.api_id].Update(source.api_ship_data.api_unset_ship);
+            }
+            
+            if (this.Ships[source.api_ship_data.api_set_ship.api_id] != null) {
+                this.Ships[source.api_ship_data.api_set_ship.api_id].Update(source.api_ship_data.api_set_ship);
+            }
+        }
+
+        #endregion
+
+        #region 工廠 (Get / Destroy)
+
+        private void GetShip(kcsapi_kdock_getship source)
 		{
 			this.homeport.Itemyard.AddFromDock(source);
 
